2b178eba56c980e28f8c11b1409b758f6fb76f61
Ehtesham Siddiqui <ehtesham@bibliocommons.com>
2019-11-02 17:51:59 -0400

    WebUI initial commit

---
+++ src/main/java/com/es/gantry/web/GantryWebController.java
@@ -0,0 +1,16 @@
+package com.es.gantry.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class GantryWebController {
+
+    @RequestMapping("/")
+    public String ui(Model model) {
+        //model.addAttribute("title","Gantry API");
+        return "gantry";
+    }
+}
